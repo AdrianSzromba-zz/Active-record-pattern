fa79e7951431d1c1c3518bf8bf3c4e7deefa9bd8
AdrianSzromba <adrian.szromba@gmail.com>
2017-11-18 13:45:43 +0100

    Add UserApp class

---
+++ src/app/UserApp.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import model.User;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		try {
+		 	conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/warsztat2?useSSL=false",
+		 			 							"root", "coderslab");
+			//User u = new User ("username","email","password");
+			//u.setUserGroupId(1);
+			//u.save(conn);
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
